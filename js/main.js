2a40d29f71ddb95a15dee51d7afe454b12f94db8
renatoirina <irinarenato793@gmail.com>
2024-04-24 23:51:38 +0200

    feat: bonus 1 completed

--- js/main.js
+++ js/main.js
@@ -1,39 +1,26 @@
-const images = [
-  {
-    image: "img/01.webp",
-    title: "Marvel's Spiderman Miles Morale",
-    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-  },
-  {
-    image: "img/02.webp",
-    title: "Ratchet & Clank: Rift Apart",
-    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-  },
-  {
-    image: "img/03.webp",
-    title: "Fortnite",
-    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-  },
-  {
-    image: "img/04.webp",
-    title: "Stray",
-    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-  },
-  {
-    image: "img/05.webp",
-    title: "Marvel's Avengers",
-    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-  },
-];
+const thumbElem = document.querySelector(".my-thumbnail");
 
-
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
   data() {
-    return{
+    return {
+      interval: setInterval(()=> {
+        if (this.isIntervalActive) {
+          if (this.isNext) {
+            this.showNext();
+          } else {
+            this.showPrev()
+          }
+        } else {
+          this.isIntervalActive = false
+        }
+        
+      }, 3000),
+      isIntervalActive: true,
       activeIndex: 0,
       greetings: "test vue",
+      isNext: false,
       images: [
         {
           image: "img/01.webp",
@@ -60,10 +47,18 @@ createApp({
           title: "Marvel's Avengers",
           text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
         },
-      ]
-    }
+      ],
+    };
+  },
+  created(){
+    this.interval
   },
   methods: {
-
+    showNext: function(){
+      this.activeIndex === this.images.length - 1 ? this.activeIndex = 0 : this.activeIndex++;
+    },
+    showPrev: function(){
+      this.activeIndex > 0 ? this.activeIndex-- : this.activeIndex = this.images.length -1;
+    }
   }
 }).mount("#app");
